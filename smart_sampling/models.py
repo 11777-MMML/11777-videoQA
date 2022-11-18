4f4c00f13cea13a6a18f86af7813fbbdba0c3b32
Tanmay Kulkarni <tanmaygk97@gmail.com>
2022-11-18 14:21:41 -0500

    create basic template

---
+++ smart_sampling/models.py
@@ -0,0 +1,22 @@
+from torch import nn
+
+class NextFramePredictor(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class FrameEmbedder(nn.Module):
+    def __init__(self):
+        pass
+    
+    def forward(self, x):
+        pass
+
+class Agent(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward():
+        pass
---
+++ smart_sampling/trainer.py
@@ -0,0 +1,10 @@
+
+class Trainer:
+    def __init__(self):
+        pass
+
+    def train_one_batch(self, batch):
+        pass
+
+    def train_one_epoch(self, epoch):
+        pass
